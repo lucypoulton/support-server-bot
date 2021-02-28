1587e3c98d4535ce1f9c7541fdc336fdaab99725
Lucy Poulton <lucy@poulton.xyz>
2021-02-28 20:58:21 +0000

    bom we're gpl now

--- src/command/channel/AbstractChannelCommand.ts
+++ src/command/channel/AbstractChannelCommand.ts
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2021 Lucy Poulton https://lucyy.me
+ * This file is part of ticketbot.
+ *
+ * ticketbot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ticketbot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ticketbot.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import {Command} from "../Command";
 import {Message, TextChannel} from "discord.js";
 import {ChannelManager} from "../../ChannelManager";
